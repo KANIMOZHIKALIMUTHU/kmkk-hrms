d8f1994fa74846bc90b78ea859fdec66a3096284
KANIMOZHIKALIMUTHU <kmkk2303@gmail.com>
2025-11-22 10:05:16 +0530

    solution

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_BASE || "https://kmkk-hrms1.onrender.com/api",
 });
 
 API.interceptors.request.use(
